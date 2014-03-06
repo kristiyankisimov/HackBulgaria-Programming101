0a8e6ef68b2621bc5c6f9d4ffbba2ee30ca1841b
kris <kris@ubuntu.(none)>
2014-03-06 17:36:22 +0200

    Adding solution and test files for problem2

--- problem2/solution.py
+++ problem2/solution.py
@@ -1,10 +1,2 @@
 def sum_of_min_and_max(arr):
 	return min(arr) + max(arr)
-
-def main():
-	print(sum_of_min_and_max([1,2,3,4,5,6,8,9]))
-	print(sum_of_min_and_max([-10,5,10,100]))
-	print(sum_of_min_and_max([1]))
-
-if __name__ == '__main__':
-	main()
