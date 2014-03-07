c7993bb398e0a3bfa549f8ea42d417f80cfe640d
kristiyankisimov <kristiyankisimov@yahoo.com>
2014-03-07 17:57:08 +0200

    Adding nth-fibonacci files

---
+++ nth-fibonacci/tests.py
@@ -0,0 +1,13 @@
+from solution import nth_fibonacci
+import unittest
+
+class NthFibonacciTest(unittest.TestCase):
+    def test_nth_fibonacci(self):
+        self.assertEqual(1, nth_fibonacci(1))
+        self.assertEqual(1, nth_fibonacci(2))
+        self.assertEqual(2, nth_fibonacci(3))
+        self.assertEqual(55, nth_fibonacci(10))
+        self.assertEqual(89, nth_fibonacci(11))
+
+if __name__ == '__main__':
+    unittest.main()
