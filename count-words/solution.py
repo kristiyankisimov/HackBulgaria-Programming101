7aee995f07a3501d40c71b2542bf3d36ef7a6bdb
kristiyankisimov <kristiyankisimov@yahoo.com>
2014-03-10 01:05:10 +0200

    Adding count-words files

---
+++ count-words/solution.py
@@ -0,0 +1,8 @@
+def count_words(arr):
+    dictionary = {}
+    for item in arr:
+        if not item in dictionary:
+            dictionary[item] = 1
+        else:
+            dictionary[item] += 1
+    return dictionarys
