13523d8e376693f853d6eb3879e0be4997594112
kristiyankisimov <kristiyankisimov@yahoo.com>
2014-03-13 08:19:01 +0200

    Adding goldbach files

---
+++ goldbach/tests.py
@@ -0,0 +1,13 @@
+from solution import goldbach
+import unittest
+
+class GoldbachTest(unittest.TestCase):
+    def test_goldbach(self):
+        self.assertEqual(goldbach(4), [(2, 2)])
+        self.assertEqual(goldbach(6), [(3, 3)])
+        self.assertEqual(goldbach(8), [(3, 5)])
+        self.assertEqual(goldbach(10), [(3, 7), (5, 5)])
+        self.assertEqual(goldbach(100), [(3, 97), (11, 89), (17, 83), (29, 71), (41, 59), (47, 53)])
+
+if __name__ == '__main__':
+    unittest.main()
