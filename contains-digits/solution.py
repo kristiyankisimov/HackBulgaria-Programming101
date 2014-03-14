b49612660c106525f71dac23edd6355bd5416174
kristiyankisimov <kristiyankisimov@yahoo.com>
2014-03-14 13:00:27 +0200

    Deleting files

--- contains-digits/solution.py
+++
@@ -1,9 +0,0 @@
-def contains_digit(number, digit):
-    while number > 0:
-        if number % 10 == digit:
-            return True
-        number //= 10
-    return False
-
-def contains_digits(number, digits):
-    return all([contains_digit(number, digit) for digit in digits])
--- groupby/solution.py
+++
@@ -1,8 +0,0 @@
-def groupby(func, seq):
-    dictionary = {}
-    for item in seq:
-        if not func(item) in dictionary:
-            dictionary[func(item)] = [item]
-        else:
-            dictionary[func(item)].append(item)
-    return dictionary
