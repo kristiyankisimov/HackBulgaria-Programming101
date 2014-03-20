d4f4bb39fa064a22bcbf01173599e94864b08d4c
kristiyankisimov <kristiyankisimov@yahoo.com>
2014-03-20 08:27:05 +0200

    Change run_tests.py

--- week0/run_tests.py
+++ week0/run_tests.py
@@ -4,11 +4,11 @@ from subprocess import call
 def run_all_tests(filename):
     for root, dirs, files in os.walk("./"):
         for f in files:
-            if f.startswith(filename):
+            if f == filename:
                 call("python3.3 " + root + "/" + f, shell=True)
 
 def main():
-    run_all_tests("tests")
+    run_all_tests("tests.py")
 
 if __name__ == '__main__':
     main()
